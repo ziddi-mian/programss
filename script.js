47a2d7baa3e1f60cfeda794093f8a0bab74d0c14
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-22 19:57:57 +0500

    jss

--- script.js
+++ script.js
@@ -84,6 +84,22 @@ dt = new Date(2015, 10, 1);
 console.log(short_months(dt));
 
 
+// Write a JavaScript function to get a full numeric representation of a year (4 digits).   
+// Test Data :
+// dt = new Date(2015, 10, 1);
+// console.log(full_year(dt));
+// 2015
+
+
+function full_year(dt) 
+{ 
+  return dt.getFullYear(); 
+}
+dt = new Date(); 
+console.log(full_year(dt)); 
+
+dt = new Date(2015, 10, 1); 
+console.log(full_year(dt));
 
 
 
