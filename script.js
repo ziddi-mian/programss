3f668f2244afbec7ceb9903fbeabd5be8803eb4f
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-22 19:55:46 +0500

    js

--- script.js
+++ script.js
@@ -47,6 +47,22 @@ dt = new Date(2015, 10, 1);
 console.log(full_month(dt));
 
 
+// Write a JavaScript function to get a numeric representation of a month, with leading zeros (01 through 12).   
+// Test Data :
+// dt = new Date(2015, 10, 1);
+// console.log(numeric_month(dt));
+// "11"
+
+function numeric_month(dt)
+{
+  return (dt.getMonth() < 9 ? '0' : '') + (dt.getMonth() + 1); 
+}
+dt = new Date(); 
+console.log(numeric_month(dt)); 
+
+dt = new Date(2015, 10, 1); 
+console.log(numeric_month(dt));
+
 
 
 
