09a61360a47be0d1d99802eea7adffef86048b4a
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-22 19:56:48 +0500

    js

--- script.js
+++ script.js
@@ -64,6 +64,25 @@ dt = new Date(2015, 10, 1);
 console.log(numeric_month(dt));
 
 
+// Write a JavaScript function to get a short textual representation of a month, three letters (Jan through Dec).   
+// Test Data :
+// dt = new Date(2015, 10, 1);
+// console.log(short_months(dt));
+// "Nov"
+
+Date.shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+function short_months(dt)
+   { 
+     return Date.shortMonths[dt.getMonth()]; 
+   }
+
+dt = new Date(); 
+console.log(short_months(dt)); 
+
+dt = new Date(2015, 10, 1); 
+console.log(short_months(dt));
+
 
 
 
