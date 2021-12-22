42b9d249cc5eec8e7b1a451ecf2cded6ea91f407
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-22 19:59:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -110,6 +110,16 @@ console.log(full_year(dt));
 // "89"
 
 
+function sort_year(dt) 
+{ 
+  return ('' + dt.getFullYear()).substr(2);
+}
+
+dt = new Date(); 
+console.log(sort_year(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(sort_year(dt));
 
 
 
